405167efb90c9af97fd7a21f6986da2a3f972663
Lauren Gould <laurenalivia18@gmail.com>
2024-11-30 17:15:22 -0600

    Adding an r script as a scratch pad/testing space

---
+++ R/testfile.R
@@ -0,0 +1,15 @@
+
+## file to test functions on example use cases
+
+
+## load libraries
+library("survival")
+library("knitr")
+library("faraway")
+
+## practice dataset will be teengamb for SLR model comparisons (using from an example hw problem in biostat methods I)
+
+lmod1<- lm(gamble ~ sex + status + income + verbal + sex:status + sex:income + sex:verbal, data = teengamb)
+lmod2<- lm(gamble ~ sex + status + income + verbal + sex:income, data = teengamb)
+
+
