3f54a5fecf90f3f8787b59131496a23ff88b701b
Lauren Gould <laurenalivia18@gmail.com>
2024-12-02 10:16:56 -0600

    re-evaluating how i want this code to work; going to rebuild it from scratch rather than what I had drafted elsewhere. Here I am attempting to iterate the extract_lm funciton internally, so that the user can just include the models in the input, rather than have to do the step of extract_lm for each prior to using this function...that will be my back up though if i cant get this to work properly

--- R/comptable.R
+++ R/comptable.R
@@ -3,8 +3,21 @@
 #'
 #'
 #' @title Generate Model Comparison Table
-#' @param model_extract model(s) to display components for
-#' @param alpha user-defined alpha; define the threshold for significance
+#' @param model_extract model(s) to display components for. can be just one, or as many as desired for side-by-side comparison of values
+#' @param alpha user-defined alpha; define the threshold for significance (brought over from previous models? is ther)
 #' @return table of relevant model components for a quick side-by-side comparison
 #'
 #'
+
+comptable<- function (..., alpha= 0.05 ) {
+
+   #group inputs from '...'
+  inputmods<- list(...)
+
+  #apply the extract_ function for each model
+  extracts<- lapply(inputmods, extract_lm)
+
+  #return results
+  return(extracts)
+
+}
