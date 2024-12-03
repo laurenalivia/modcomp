44bb53f62a84e83b3d0086134394cdca8507cd4c
Lauren Gould <laurenalivia18@gmail.com>
2024-12-03 06:57:32 -0600

    to dispell my own returning confusion, making a slight difference and calling this param 'alpha_' becuase I find it sketchy to code 'alpha=alpha' when passing the parameter from the outter function to the inner. This is likely not an issue, but makes more sense to me as I come back to this to understand what is going on with the slight difference in name between the parameters. It does work the other way, "alpha=alpha", so I might come back and just do that, but for now this is easier for me to understand, and I think it would make more sense for someone reviewing the code as well?

--- R/tablecomp.R
+++ R/tablecomp.R
@@ -1,3 +1,36 @@
 #want to put the extracted model components in a table for side-by-side comparison with each model it's own column, predictors on the right, and something where you
   #user chooses the values placed in each cell, like: [coef estimate] concatenated with the pvalue and stars? With the option to instead have the output value be confintervals, etc?
   #return(extract)
+#' function to generate a table for easy comparison against similar or nested models.
+#'
+#'
+#' @title Generate Model Comparison Table
+#' @param model_extract model(s) to display components for. can be just one, or as many as desired for side-by-side comparison of values
+#' @param modeltype 'lm' for linear model, 'coxph' for cox proportional hazards model.
+#' @return table of relevant model components for a quick side-by-side comparison
+#'
+#'
+
+tablestack<- function (..., modeltype= c("lm", "coxph")) {
+
+  #Validate that modeltype specified is either 'lm' or 'coxph' using 'match.arg()' function
+  modeltype<- match.arg(modeltype)
+
+  #group the multiple model inputs from '...' so they can be iterated over for functions later
+  inputmods<- list(...)
+
+  #figured out the 'if-stop' usage for a piece, rather than a tryCatch or stopifnot:
+  #make sure they are all the same type, and that type matches what was specified for 'modeltype' in the input
+  if(!all(sapply(inputmods, inherits, what= modeltype))) {
+    stop("All models supplied must be of type specified:", modeltype)
+  }
+  #if they are all the same, this will be printed to the output, "\n" forces new line after this message for remaining output
+  cat("All models are of type specified:", modeltype, "\n")
+
+  #apply the extract_ function for each model in the grouped list
+  extracts<- lapply(inputmods, extract_lm)
+
+  #output is the model summaries stacked right on top of each other, so the user doesnt have to scroll as much, and has everything displayed at once
+  return(kable(extracts))
+
+}
