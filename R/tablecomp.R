b12f26c516df3f4878e9f69cddece77a6b85f494
Lauren Gould <laurenalivia18@gmail.com>
2024-12-02 20:33:22 -0600

    this will be the function, tablecomp, where I try to actualize my initial vision of having the parameters listed to the left, the models as columns, and values user defined, with the default being something like [coef estimate, pval concat star]..

---
+++ R/tablecomp.R
@@ -0,0 +1,3 @@
+#want to put the extracted model components in a table for side-by-side comparison with each model it's own column, predictors on the right, and something where you
+  #user chooses the values placed in each cell, like: [coef estimate] concatenated with the pvalue and stars? With the option to instead have the output value be confintervals, etc?
+  #return(extract)
