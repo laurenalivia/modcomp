f1bd098b2672fcdca82ced2152f04aeb691fa5aa
Lauren Gould <laurenalivia18@gmail.com>
2024-12-03 12:37:53 -0600

    update documentation about how to distinguish the tables, since I am struggling to get the model names to appear appropriately in the tables. This is a good thing to put in the accompanying report as something to work on for future versions of this package.

--- R/tablestack.R
+++ R/tablestack.R
@@ -6,7 +6,7 @@
 #' @param model(s) model(s) to display components for. can be just one, or as many as desired for a comparison of values
 #' @param modeltype 'lm' for linear model, 'coxph' for cox proportional hazards model.
 #' @param alpha_ user-defined alpha; define the threshold for significance. Default is 0.05.
-#' @return table(s) of relevant model components for a quick comparison.
+#' @return table(s) of relevant model components for a quick comparison. They are displayed in the order they are entered as input, with the top being the first model specified, and so on.
 #' @examples
 #' #fit linear models
 #' lmod1<- lm(gamble ~ sex + status + income + verbal + sex:status + sex:income + sex:verbal, data = teengamb)
